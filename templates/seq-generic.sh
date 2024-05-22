b5a1ca4b668241e4360f404392216fe1312cd43a
Nicolas Rühling <82144991+nrueh@users.noreply.github.com>
2024-05-22 16:21:41 -0300

    added files from klaus

--- templates/seq-generic.sh
+++ templates/seq-generic.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 # http://www.cril.univ-artois.fr/~roussel/runsolver/
 
-CAT="{run.root}/programs/gcat.sh"
-
 cd "$(dirname $0)"
 
 #top -n 1 -b > top.txt
 
-[[ -e .finished ]] || $CAT "{run.file}" | "{run.root}/programs/runsolver-3.3.4" \
-	-M 20000 \
-	-w runsolver.watcher \
-	-o runsolver.solver \
-	-W {run.timeout} \
-	"{run.root}/programs/{run.solver}" {run.args}
+[[ -e .finished ]] || "{run.root}/programs/runlim" \
+	--output-file=runsolver.watcher --space-limit=16000 \
+	--time-limit={run.timeout} \
+	"{run.root}/programs/{run.solver}" {run.args} {run.file} > runsolver.solver
 
 touch .finished
